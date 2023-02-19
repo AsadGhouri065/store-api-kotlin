8e5b5993ddb11486b5298ad66413e9830de0577b
M. Asad Ghouri <asadghouri1994@hotmail.com>
2023-02-19 18:16:10 +0500

    initial commit

---
+++ src/main/kotlin/com/api/store/amazon/StoreServiceKtApplication.kt
@@ -0,0 +1,11 @@
+package com.api.store.amazon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StoreServiceKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<StoreServiceKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/api/store/amazon/StoreServiceKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.store.amazon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StoreServiceKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
